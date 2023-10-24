19a719872236449641b44abf7a6a2f968a73fc07
Mani Poorna <ndmprao2001@gmail.com>
2023-10-24 11:59:32 +0530

    V 1.1

--- src/components/Itemlist.js
+++ src/components/Itemlist.js
@@ -6,9 +6,11 @@ const Itemlist = () => {
   return (
     <div>
       <h1>Item List</h1>
-      <Link to="/items/1">Item 1</Link><br></br>
-      <Link to="/items/2">Item 2</Link><br></br>
-      <Link to="/items/3">Item 3</Link><br></br>
+      <ul>
+        <li><Link to="/items/1">Item 1</Link></li>
+        <li><Link to="/items/2">Item 2</Link></li>
+        <li><Link to="/items/3">Item 3</Link></li>
+      </ul>
     </div>
   )
 }
